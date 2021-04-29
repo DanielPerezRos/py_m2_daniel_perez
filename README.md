2718c8f640a9cde92e0e3f15c390a924fcd88fca
DanielPerezRos <ahhrrc@gmail.com>
2021-04-29 19:57:29 +0200

    Commit inicial

---
+++ README.md
@@ -0,0 +1,18 @@
+# Aplicación CRUD, que lea datos de ordenadores por consola y los almacene en una estructura de datos. 
+# La aplicación ofrece las siguientes opciones:
+
+1. – Consultar ordenadores: devuelve los ordenadores que tiene guardados en una estructura de datos.
+
+2. – Consultar un ordenador: solicita el id del ordenador y en base a eso se muestran los datos
+
+3. – Crear un nuevo ordenador: pide los datos de un ordenador (id, fabricante, modelo, precio, RAM, peso)
+
+4. – Editar un ordenador: pide el id del ordenador y después todos los datos para sobreescribir los que ya tenía para ese ordenador.
+
+5. – Borrar un ordenador: pide el id del ordenador y lo borra de la estructura de datos.
+
+6. – Salir: rompe el bucle y termina la aplicación.
+
+### Con un bucle se pide al usuario que introduzca una opción (por el número de opción) y en base a la  
+### opción se realiza una operación o se preguntan más datos para poder llevar a cabo la acción elegida. 
+### Se imprime en terminal los resultados de la acción solicitada.
---
+++ py_m2_daniel_perez.py
@@ -0,0 +1,52 @@
+laptops = []  # Lista
+
+def create_laptop():
+    laptop = dict()
+    laptop["id"] = int(input("Introduce el id "))
+    laptop["fabricante"] = input("Introduce el fabricante ")
+    laptops.append(laptop)
+    print("Ordenador registrado")
+
+def find_laptop(id_laptop):
+    for laptop in laptops:
+        if laptop["id"] == id_laptop:
+            return laptop
+
+while True:
+    print("""
+    Menú de opciones: 
+    1 - Consultar ordenadores
+    2 - Consultar un ordenador
+    3 - Crear un ordenador
+    4 - Editar
+    5 - borrar
+    6 - Salir
+    """)
+    option = int(input("Selecciona una opción: "))
+    if option == 1:
+        print(laptops)
+    elif option == 2:
+        if len(laptops) == 0:
+            print("No hay ordenadores disponibles")
+            continue
+
+
+        id_laptop = int(input("introduce el id "))
+
+        laptop = find_laptop(id_laptop)
+        if laptop is not None:
+            print(laptop)
+        else:
+            print("No encontrado")
+
+
+    elif option == 3:
+        create_laptop()
+    elif option == 4:
+        print("Has elegido 4")
+    elif option == 5:
+        print("Has elegido 5")
+    elif option == 6:
+        break
+
+print("fin")
